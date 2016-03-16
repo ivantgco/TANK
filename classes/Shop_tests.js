eea8703de338595476172cb08bc6c358c6571c19
ivantgca <ivantgca@gmail.com>
2016-03-16 18:57:00 +0300

    Shop_tests + widget ---> начало

--- classes/Shop_tests.js
+++ classes/Shop_tests.js
@@ -3,9 +3,20 @@
  */
 var MyError = require('../error').MyError;
 var Widget = require('./shop_tests/Widget');
+var request = require('request');
+var moment = require('moment');
+var async = require('async');
+var funcs = require('../libs/functions');
+
 
 var Model = function(obj){
     this.name = obj.name;
+    this.host = obj.host || 'https://shop.mirbileta.ru/cgi-bin/b2e';
+    this.widgetes = [];
+    this.widgetesCount = 0;
+    this.max_widgetes_count = 0;
+    this.actions_list = [];
+    this.actions = [];
 };
 
 Model.prototype.init = function (obj, cb) {
@@ -19,6 +30,121 @@ Model.prototype.init = function (obj, cb) {
     cb(null);
 
 };
+Model.prototype.request = function (obj, cb) {
+    var _t = this;
+    obj.params.frame = obj.params.frame || _t.frame;
+    var query = funcs.makeQuery(obj);
+    request(_t.host + '?request=' + query, {}, function (err, res) {
+        if (err) {
+            return cb(new MyError('Во время запроса произошел сбой', {query:query,err:err}));
+        }
+        if (res.statusCode!==200){
+            return cb(new MyError('Запрос вернулся с неверным кодом: ' + res.statusCode, {query:query}));
+        }
+        try {
+            var data = JSON.parse(res.body);
+            data = data.results[0];
+        } catch (e) {
+            console.log(e);
+            return cb(new MyError('Запрос вернул не корректный JSON',{query:query,res:res.body}));
+        }
+        if (+data.code){
+            console.log(data);
+            return cb(new MyError('Логическая ошибка.',{query:query,data:data}));
+        }
+        return cb(null, data);
+    });
+};
+Model.prototype.start = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    if (typeof cb !== 'function') throw new MyError('В метод не передан cb');
+    if (typeof obj !== 'object') return cb(new MyError('В метод не переданы obj'));
+    var _t = this;
+    _t.host = obj.host || _t.host;
+    _t.frame = obj.frame;
+    if (!_t.frame) return cb(new MyError('Необходимо передавать фрейм'));
+    _t.max_widgetes_count = obj.max_widgetes_count || _t.max_widgetes_count || 0;
+
+    _t.actions_list = (Array.isArray(obj.actions_list))? obj.actions_list : _t.actions_list || [];
+    // надо загрузить
+    var o = {
+        command:'get_actions',
+        params:{
+            action_id:_t.actions_list.split
+        }
+    };
+    _t.request(o, function (err, res) {
+        if (err) return cb(err);
+        var objData = funcs.jsonToObj(res);
+        for (var i in objData) {
+            _t.actions.push(objData[i]);
+        }
+        _t.timeStart = moment();
+        _t.sync();
+    });
+    //_t.sync(obj);
+};
+Model.prototype.sync = function (obj) {
+    obj = obj || {};
+    var _t = this;
+    _t.max_widgetes_count = obj.max_widgetes_count || _t.max_widgetes_count || 0;
+
+    // Соберем статистическую информацию
+    // Запускаем необходимое количество процессов
+    //-------------------------------------------
+    // посчитаем текущее количество виджетов
+    _t.widgetesCount = 0;
+    var requestsCount = 0;
+
+    var errorRequests = 0;
+    var loaded = 0;
+    for (var i in _t.widgetes) {
+        if (!_t.widgetes[i].stop) _t.widgetesCount++;
+        requestsCount += _t.widgetes[i].counters.requests;
+        errorRequests += _t.widgetes[i].counters.errorRequests;
+        loaded += _t.widgetes[i].counters.loaded;
+    }
+    var timeCycle = moment();
+    console.log('Запущено виджетов', _t.widgetesCount);
+    console.log('Загружено виджетов (раз):', loaded);
+    console.log('Выполнено запросов:', requestsCount);
+    console.log('С ош.:', errorRequests);
+    var timeAgo = (timeCycle - _t.timeStart)/1000;
+    console.log('Запросов в секунду СРЕДНЕЕ:', requestsCount / timeAgo);
+    var requestsCountMoment = requestsCount - (_t.requestsCountOld || 0);
+    _t.requestsCountOld = requestsCount;
+    console.log('Запросов в секунду МОМЕНТАЛЬНОЕ:', requestsCountMoment);
+    console.log('Виджетов в секунду ===СРЕДНЕЕ======>', loaded / timeAgo);
+    console.log('_____________ '+ timeCycle.format('HH:mm:ss') +'_______Прошло: '+ timeAgo +' сек._______________\n');
+    var needToStart = _t.max_widgetes_count - _t.widgetesCount;
+    var widgetesTmp = [];
+    for (var c=0; c<needToStart; c++){
+        var action = _t.actions[Math.round((Math.random() * _t.actions.length - 1))];
+        if (!action) continue;
+        var o = {
+            host: _t.host,
+            frame: _t.frame,
+            action: action
+        };
+        var w1 = new Widget(o);
+        widgetesTmp.push(w1);
+    }
+    async.each(widgetesTmp, function (item, cb) {
+         setTimeout(function () {
+             _t.widgetes.push(item);
+             item.start();
+             cb(null);
+         }, Math.round(Math.random()*1000 * widgetesTmp.length));
+    }, function (err) {
+        if (err) throw err;
+        setTimeout(function () {
+            _t.sync();
+        },1000)
+    });
+};
 Model.prototype.one = function (obj, cb) {
     if (arguments.length == 1) {
         cb = arguments[0];
@@ -27,15 +153,18 @@ Model.prototype.one = function (obj, cb) {
     if (typeof cb !== 'function') throw new MyError('В метод не передан cb');
     if (typeof obj !== 'object') return cb(new MyError('В метод не переданы obj'));
     var _t = this;
-    var w1 = new Widget({});
-    w1.request({
-        command:'get_action_sectors',
-        object:'',
-        params:{
-            action_id:'1237'
-        }
-    }, function (err, res) {
-        console.log('ERR', err);
-    })
+    //
+    //var w1 = new Widget({action_id:2978});
+    var w1 = new Widget({action_id:1237});
+    w1.start();
+    //w1.request({
+    //    command:'get_action_sectors',
+    //    object:'',
+    //    params:{
+    //        action_id:'1237'
+    //    }
+    //}, function (err, res) {
+    //    console.log('ERR', err);
+    //})
 };
 module.exports = Model;
--- classes/shop_tests/Widget.js
+++ classes/shop_tests/Widget.js
@@ -4,22 +4,34 @@
 var MyError = require('../../error').MyError;
 var request = require('request');
 var moment = require('moment');
+var async = require('async');
 var funcs = require('../../libs/functions');
 
 
 var Widget = function (params) {
     if (typeof params!=='object') params = {};
-    this.maxRequests = params.maxRequests || 20;
-    this.host = params.host || 'https://shop.mirbileta.ru/cgi-bin/b2e';
-    this.frame = params.frame || 'sdfsd9f8uorj83421o901hndhfs78df5t61yu4g1dck3h1239186yhoi';
+    this.maxRequests = params.maxRequests || 0;
+    this.host = params.host;
+    this.frame = params.frame;
+    this.action = params.action;
+    if (!this.host || !this.frame || !this.action) {
+        console.log('action',this.action);
+        console.log('host',this.host);
+        console.log('frame',this.frame);
+        throw new MyError('В Widget не переданы frame, host или action',{host:this.host, frame:this.frame, action_id:this.action});
+    }
+    this.action_id = params.action_id;
+    this.wait_max_time = params.wait_max_time || 1;
     this.counters = {
         requests:0,
+        errorRequests:0,
         loaded: 0,
         order_created: 0,
-        queries:{}
+        queries:{},
+        cycles:0
     };
     this.queries_time = {
-
+        loading:[]
     }
 };
 Widget.prototype.request = function (obj, cb) {
@@ -30,30 +42,266 @@ Widget.prototype.request = function (obj, cb) {
     var t1 = moment();
     if (typeof obj.params!=='object') obj.params = {};
     obj.params.frame = obj.params.frame || _t.frame;
-    var alias = obj.command + '.' + obj.object || '';
+    var alias = obj.command + '.' + (obj.object || '');
     var query = funcs.makeQuery(obj);
     request(_t.host + '?request=' + query, {}, function (err, res) {
-        if (err) return cb(new MyError('Во время запроса произошел сбой', {query:query,err:err}));
+        _t.counters.requests++;
+        if (typeof _t.counters.queries[alias]!=='object') _t.counters.queries[alias] = 0;
+        _t.counters.queries[alias]++; // увеличим счетчик конкретного запроса
+        if (typeof _t.queries_time[alias]!=='object') _t.queries_time[alias] = [];
+        _t.queries_time[alias].push(moment()-t1);
+        if (err) {
+            _t.counters.errorRequests++;
+            return cb(new MyError('Во время запроса произошел сбой', {query:query,err:err}));
+        }
         if (res.statusCode!==200){
+            _t.counters.errorRequests++;
             return cb(new MyError('Запрос вернулся с неверным кодом: ' + res.statusCode, {query:query}));
         }
         try {
             var data = JSON.parse(res.body);
             data = data.results[0];
-            data = funcs.jsonToObj(data);
         } catch (e) {
             console.log(e);
             return cb(new MyError('Запрос вернул не корректный JSON',{query:query,res:res.body}));
         }
-        console.log(data);
+        if (+data.code){
+            console.log(data);
+            return cb(new MyError('Логическая ошибка.',{query:query,data:data}));
+        }
+        return cb(null, data);
+
     });
 
 };
 
+
+/**
+ * Рекурсивная функция.
+ */
 Widget.prototype.start = function () {
     var _t = this;
     if (_t.stop) return;
+    // Вызвать загрузку виджета до мест на схеме
+    // Если благоприятные условия -> создать заказ
+    // Подождать
+    // Повторить
+    async.series([
+        function (cb) {
+            // Вызвать загрузку виджета до мест на схеме
+            _t.loadScheme(function (err) {
+                if (err){
+                    _t.stop = err;
+                }
+                return cb(err);
+            });
+        },
+        function (cb) {
+            // Если благоприятные условия -> создать заказ
+            return cb(null);
+        },
+        function (cb) {
+            // Подождать
+            var random_time = Math.round(Math.random(_t.wait_max_time));
+            setTimeout(function () {
+                cb(null);
+            },random_time);
+        }
+    ], function (err) {
+        // Повторить
+        _t.counters.cycles++;
+        if (err){
+            if (!_t.stop) _t.stop = err;
+            return;
+        }
+        //console.log('Выполнено раз:',_t.counters.cycles);
+        if (_t.counters.cycles < _t.maxRequests || _t.maxRequests==0) return _t.start();
+        //console.log('Счетчики:',_t.counters);
+        //console.log('Время:',_t.queries_time);
+        //console.log('Завершено.');
+    })
+};
 
+Widget.prototype.loadScheme = function (obj, cb) {
+    if (arguments.length == 1) {
+        cb = arguments[0];
+        obj = {};
+    }
+    if (typeof cb !== 'function') throw new MyError('В метод не передан cb');
+    if (typeof obj !== 'object') return cb(new MyError('В метод не переданы obj'));
+    var _t = this;
+    var t1 = moment();
+    async.series([
+        function (cb) {
+            // Загрузим информацию по мероприятию <query><action_id>2983</action_id><frame>asdasd09bno245fmme</frame><command>get_actions</command></query>
+            var o = {
+                command: 'get_actions',
+                params: {
+                    action_id: _t.action.ACTION_ID
+                }
+            };
+            _t.request(o, function (err, res) {
+                if (err) return cb(err);
+                var dataObj = funcs.jsonToObj(res);
+                if (funcs.countObj(dataObj)==0) return cb(new MyError('Нет такого мероприятия'));
+                _t.action = dataObj[0];
+                cb(null);
+            });
+        },
+        function (cb) {
+            // Если мероприятие разбивается на сектора, то загрузим сектора, иначе сразу схему
+            //SPLIT_BY_AREA_GROUP
+            if (_t.action.SPLIT_BY_AREA_GROUP == 'TRUE'){
+                async.series([
+                    function (cb) {
+                        // Загрузим сектора
+                        //<query><action_id>1458</action_id><frame>kjhsdfsd87789sdfjs734j238dsj834g234i58skdu4y3278gyujwe7r3</frame><command>get_action_sectors</command></query>
+                        //console.log('Загрузим сектора');
+                        var o = {
+                            command: 'get_action_sectors',
+                            params: {
+                                action_id: _t.action.ACTION_ID
+                            }
+                        };
+                        _t.request(o, function (err, res) {
+                            if (err) return cb(err);
+                            var dataObj = funcs.jsonToObj(res);
+                            if (funcs.countObj(dataObj)==0) return cb(new MyError('Нет секторов на мероприятие.'));
+                            _t.action.sectors = dataObj;
+
+                            // Загрузим слои и объекты для сектора
+                            var o = {
+                                command: 'get_action_scheme_layer',
+                                params: {
+                                    action_id: _t.action.ACTION_ID,
+                                    VISIBLE: 'SECTOR'
+                                }
+                            };
+                            _t.request(o, function (err, res) {
+                                if (err) return cb(err);
+                                var dataObj = funcs.jsonToObj(res);
+                                if (funcs.countObj(dataObj)==0) {
+                                    _t.action.layers = {};
+                                    return cb(null);
+                                }
+                                _t.action.layers = dataObj;
+                                async.eachSeries(_t.action.layers, function (item, cb) {
+                                    var o = {
+                                        command: 'get_action_scheme_object',
+                                        params: {
+                                            action_id: _t.action.ACTION_ID,
+                                            VISIBLE: 'SECTOR'
+                                        }
+                                    };
+                                    _t.request(o, function (err, res) {
+                                        if (err) return cb(err);
+                                        var dataObj = funcs.jsonToObj(res);
+                                        if (funcs.countObj(dataObj)==0) {
+                                            _t.action.objects = {};
+                                            return cb(null);
+                                        }
+                                        _t.action.objects = dataObj;
+                                        cb(null);
+                                    });
+                                }, cb);
+                                cb(null);
+                            });
+
+                        });
+                    },
+                    function (cb) {
+                        // загрузим случайный сектор где есть доступные места // FREE_PLACES
+                        //console.log('загрузим случайный сектор где есть доступные места');
+                        _t.activeSectors = [];
+                        for (var i in _t.action.sectors) {
+                            if (_t.action.sectors[i].FREE_PLACES > 0) _t.activeSectors.push(_t.action.sectors[i]);
+                        }
+                        var sectorsCount = _t.activeSectors.length;
+                        if (!sectorsCount) return cb(new MyError('Нет мест на мероприятие.'));
+                        var sector_num = Math.round(Math.random()*(sectorsCount-1));
+                        //<query><action_id>1458</action_id><frame>kjhsdfsd87789sdfjs734j238dsj834g234i58skdu4y3278gyujwe7r3</frame><area_group_id>6171</area_group_id><command>get_action_scheme</command></query>
+                        var o = {
+                            command: 'get_action_scheme',
+                            params: {
+                                action_id: _t.action.ACTION_ID,
+                                area_group_id: _t.activeSectors[sector_num].AREA_GROUP_ID
+                            }
+                        };
+                        _t.request(o, function (err, res) {
+                            if (err) return cb(err);
+                            var dataObj = funcs.jsonToObj(res);
+                            if (funcs.countObj(dataObj)==0) return cb(new MyError('Нет мест на мероприятие.'));
+                            _t.action.squares = dataObj;
+                            cb(null);
+                        });
+                    }
+                ], cb);
+            }else{
+                // Просто загрузим схему <query><action_id>2983</action_id><frame>asdasd09bno245fmme</frame><command>get_action_scheme</command></query>
+                //console.log('Просто загрузим схему');
+                var o = {
+                    command: 'get_action_scheme',
+                    params: {
+                        action_id: _t.action.ACTION_ID
+                    }
+                };
+                _t.request(o, function (err, res) {
+                    if (err) return cb(err);
+                    var dataObj = funcs.jsonToObj(res);
+                    if (funcs.countObj(dataObj)==0) return cb(new MyError('Нет мест на мероприятие.'));
+                    _t.action.squares = dataObj;
+                    cb(null);
+                });
+            }
+        },
+        function (cb) {
+            // Загрузим слои и объекты
+            //<query><action_id>2436</action_id><frame>kjhsdfsd87789sdfjs734j238dsj834g234i58skdu4y3278gyujwe7r3</frame><VISIBLE>IFRAME</VISIBLE><command>get_action_scheme_layer</command></query>
+            //<query><action_id>2436</action_id><VISIBLE>IFRAME</VISIBLE><ACTION_SCHEME_LAYER_ID>1807</ACTION_SCHEME_LAYER_ID><command>get_action_scheme_object</command></query>
+            //console.log('Загрузим слои и объекты. Последнее действие.');
+            var o = {
+                command: 'get_action_scheme_layer',
+                params: {
+                    action_id: _t.action.ACTION_ID,
+                    VISIBLE: 'IFRAME'
+                }
+            };
+            _t.request(o, function (err, res) {
+                if (err) return cb(err);
+                var dataObj = funcs.jsonToObj(res);
+                if (funcs.countObj(dataObj)==0) {
+                    _t.action.layers = {};
+                    return cb(null);
+                }
+                _t.action.layers = dataObj;
+                async.eachSeries(_t.action.layers, function (item, cb) {
+                    var o = {
+                        command: 'get_action_scheme_object',
+                        params: {
+                            ACTION_ID: _t.action.ACTION_ID,
+                            ACTION_SCHEME_LAYER_ID: item.ACTION_SCHEME_LAYER_ID,
+                            VISIBLE: 'IFRAME'
+                        }
+                    };
+                    _t.request(o, function (err, res) {
+                        if (err) return cb(err);
+                        var dataObj = funcs.jsonToObj(res);
+                        if (funcs.countObj(dataObj)==0) {
+                            _t.action.objects = {};
+                            return cb(null);
+                        }
+                        _t.action.objects = dataObj;
+                        cb(null);
+                    });
+                }, cb);
+            });
+        }
+    ], function (err) {
+        if (err) return cb(err);
+        _t.counters.loaded++;
+        _t.queries_time.loading.push(moment()-t1);
+        cb(null);
+    });
 };
 
 module.exports = Widget;
